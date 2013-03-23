e9995073f97fae41e9b9acec8cec2b6f0dd5194a
Rick Pastoor <rick@wrep.nl>
2013-03-23 14:57:57 +0100

    Check in first version

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-infinitous
-==========
+Infinitus
+=========
 
-A super tiny jQuery plugin to implement infinite scrolling
+A super tiny jQuery plugin to implement infinite scrolling.
+
+Basic usage
+===========
+
+Attach the infinitus listener to an object in your page. Provide a `trigger` callback. Inifinitus passes a `done` handler to your trigger callback. You should call this handler as soon as you have finished loading and presenting data.
+
+``` javascript
+$('#main').infinitus({
+	trigger : function(done) {
+		setTimeout(function() {
+			$('#main').append('<div><p>Your loaded content.</p></div>');
+			done();
+		}, 2000);
+	},
+	onStart : function() {
+		$('.loading').fadeIn();
+	},
+	onEnd : function() {
+		$('.loading').fadeOut();
+	}
+});
+```
+
+Contributing
+============
+
+Please submit any issues or feature requests to our issue tracker!
---
+++ infinitus.jquery.js
@@ -0,0 +1,70 @@
+/*
+ *  Project: Infinitus Scroll
+ *  Description: A super tiny jQuery plugin to implement infinite scrolling.
+ *  Author: Rick Pastoor (rick@wrep.nl)
+ *  License: MIT
+ */
+
+;(function ($, window, document, undefined) {
+
+	// Set up defaults
+    var pluginName = "infinitus";
+    var defaults = {
+        trigger: $.noop,
+        onStart: $.noop,
+        onEnd: $.noop
+    };
+
+    /**
+     * Construct Infinitus
+     */
+    function Infinitus(element, options) {
+        this.element = $(element);
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.window = $(window);
+        this.working = false;
+        this.init();
+    }
+
+    Infinitus.prototype = {
+        init: function() {
+        	var self = this;
+        	self.window.on('scroll', function() {
+        		if (!self.working && self.isAtBottom()) {
+        			// Start working
+        			self.working = true;
+        			
+        			// Call onStart
+        			self.options.onStart();
+        			
+        			self.options.trigger(function() {
+        				// End working
+        				self.working = false;
+        				
+        				// Call onEnd
+            			self.options.onEnd();
+        			});
+        		}
+        	});
+        },
+        
+        isAtBottom: function() {
+        	var contentOffset = this.element.offset(),
+				elementBottom = this.element.height() + contentOffset.top,
+				scrollBottom = this.window.scrollTop() + this.window.height();
+
+        	return scrollBottom >= elementBottom;
+        }
+    };
+
+    $.fn[pluginName] = function(options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Infinitus(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
